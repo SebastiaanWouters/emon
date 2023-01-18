8a9401dc8815901bf4f8351567536814c861c456
Sebastiaan <sebastiaan.wouters@student.kuleuven.be>
2023-01-18 21:40:46 +0100

    add npub support to search + validate user input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,51 +2,67 @@ import React, { useEffect } from 'react'
 import User from '../img/user.png'
 import Add from '../img/new.svg'
 import { useNostrEvents, useProfile } from 'nostr-react'
-import { useContext, useState } from 'react'
+import { useContext, useState, useRef } from 'react'
 import { chatContext } from '../contexts/useChatContext'
 import { userContext } from '../contexts/useUserContext'
 import { navigationContext } from '../contexts/useNavigationContext'
 import { SearchPreview } from './SearchPreview'
 import { fetchMetaData } from '../utils/utils'
+import { nip19 } from 'nostr-tools'
+import assert from 'assert';
 
 export const Search = () => {
 
   const { pubkey } = useContext(userContext);
   const { setIsSearching } = useContext(navigationContext);
   const { setCurrentUserPubkey, setCurrentUserName, setCurrentUserPicture } = useContext(chatContext);
-  const [ searchedPubkey, setSearchedPubkey ] = useState("");
-  const [ searchedUserData, setSearchedUserData ] = useState(null);
+  const [ enteredPubkey, setEnteredPubkey ] = useState("");
+  const [ pubkeyToSearch, setPubkeyToSearch] = useState("");
 
+  const { data: userData } = useProfile({
+    pubkey: pubkeyToSearch
+  })
 
   useEffect(() => {
 
     async function updateSearchedData() {
-      fetchMetaData(searchedPubkey, setSearchedUserData);
+      if (enteredPubkey.slice(0,4) === "npub" && enteredPubkey.length === 63) {
+        try {
+          const { data : pk } = nip19.decode(enteredPubkey);
+          setPubkeyToSearch(pk);
+        } catch {
+        }
+        
+      } else if (enteredPubkey.length === 64 && enteredPubkey.match(/[0-9A-Fa-f]{6}/g)) {
+        setPubkeyToSearch(enteredPubkey);
+      } else {
+        setPubkeyToSearch("")
+      }
     }
     
     updateSearchedData();
     
-  }, [searchedPubkey])
+  }, [enteredPubkey])
   
 
   const addUser = () => {
-    setCurrentUserPubkey(searchedUserData.pubkey);
+    setCurrentUserPubkey(pubkeyToSearch);
     try {
-      setCurrentUserPicture(JSON.parse(searchedUserData.content).picture ? encodeURI(JSON.parse(searchedUserData.content).picture) : `https://api.dicebear.com/5.x/avataaars/png?seed=${searchedUserData.pubkey.slice(0,5)}`);
+      setCurrentUserPicture(userData.picture ? encodeURI(userData.picture) : `https://api.dicebear.com/5.x/avataaars/png?seed=${userData.pubkey.slice(0,5)}`);
     } catch {
-      setCurrentUserPicture(`https://api.dicebear.com/5.x/avataaars/png?seed=${searchedUserData.pubkey.slice(0,5)}`);
+      setCurrentUserPicture(`https://api.dicebear.com/5.x/avataaars/png?seed=${userData.pubkey.slice(0,5)}`);
     }
     
-    setCurrentUserName(JSON.parse(searchedUserData.content).name ? JSON.parse(searchedUserData.content).name : `E${searchedUserData.pubkey}` ),
+    setCurrentUserName(userData.name ? userData.name : `E${searchedUserData.pubkey}` ),
     setIsSearching(false);
   }
 
   return (
     <div className="search">
         <div className="searchForm">
-            <input type="text" onChange={(e) => {setSearchedPubkey(e.target.value)}} placeholder="enter pubkey" />
+            <input type="text" onChange={(e) => {setEnteredPubkey(e.target.value)}} placeholder="enter pubkey" />
         </div>
-        {searchedUserData && <SearchPreview pk={searchedPubkey} pic={JSON.parse(searchedUserData.content).picture ? JSON.parse(searchedUserData.content).picture : `https://api.dicebear.com/5.x/avataaars/png?seed=${searchedPubkey.slice(0,5)}`} name={JSON.parse(searchedUserData.content).name ? JSON.parse(searchedUserData.content).name : `E${searchedPubkey.slice(0,5)}` } onClick={addUser}></SearchPreview>}
+        {userData && <SearchPreview pk={pubkeyToSearch} pic={userData.picture ? userData.picture : `https://api.dicebear.com/5.x/avataaars/png?seed=${pubkeyToSearch.slice(0,5)}`} name={userData.name ? userData.name : `E${pubkeyToSearch.slice(0,5)}` } onClick={addUser}></SearchPreview>}
     </div>
   )
 }
