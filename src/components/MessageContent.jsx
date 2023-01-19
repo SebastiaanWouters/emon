a5d1166f4744dbb38dea7b9b5cc859540b8dd352
Sebastiaan <sebastiaan.wouters@student.kuleuven.be>
2023-01-19 17:22:35 +0100

    add support for image links

--- src/components/MessageContent.jsx
+++ src/components/MessageContent.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import reactStringReplace from 'react-string-replace'
 
 export const MessageContent = ({msg}) => {
-
+    const IMAGE_PROXY = "https://imgproxy-prod-emon-image-proxy-8tbihf.mo2.mogenius.io/imgproxy-og28dq/plain/";
     const linkRegex = /(https?:\/\/\S+)/g
     const lnRegex =
       /(lightning:[\w.-]+@[\w.-]+|lightning:\w+\?amount=\d+|(?:lightning:)?(?:lnurl|lnbc|lntb)[\da-z0-9]+)/gi;
+
+    const imageRegexp = /(https?:\/\/.*\.(?:png|jpg|gif))/i;
+
+    function replaceImages(msg) {
+      return (reactStringReplace(msg, imageRegexp, (match, i) => (
+          <img key={match + i} src={`${IMAGE_PROXY}${match}`}>
+          </img>
+        )))
+  }
+
 
     function replaceLinks(msg) {
         return (reactStringReplace(msg, linkRegex, (match, i) => (
@@ -24,8 +34,11 @@ export const MessageContent = ({msg}) => {
                     </>)
           });
     }
+
+
     function replaceAll(msg) {
-        const replacedLinks = replaceLinks(msg);
+      const replacedImages = replaceImages(msg);
+        const replacedLinks = replaceLinks(replacedImages);
         const replacedInvoices = replaceInvoice(replacedLinks);
         return replacedInvoices;
     }
