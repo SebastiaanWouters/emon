afa36c060cff43c0c6d361bce2973b3c24294f02
Sebastiaan <sebastiaan.wouters@student.kuleuven.be>
2023-01-21 17:50:41 +0100

    add toast message when no lightning plugin is installed

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,20 +9,49 @@ import { userContext } from '../contexts/useUserContext';
 import { useContext, useState } from 'react';
 import { useNostrEvents, useProfile } from 'nostr-react'
 import { InvoiceCreator } from './InvoiceCreator'
+import toast, { Toaster, ToastBar } from 'react-hot-toast';
 
 export const Chat = () => {
   const { currentUserPubkey, currentUserName, currentUserPicture } = useContext(chatContext);
   const [invoiceActive, setInvoiceActive] = useState(false)
+  const noExt = () => toast(
+    (t) => (
+    <span>
+        <button className="hover:text-purple-900" onClick={() => toast.dismiss(t.id)}><a href="https://getalby.com" target="_blank">You need a lightning enabled browser extension to send and receive sats, click here to learn more</a></button>
+    </span>
+    ),
+    {
+    duration: 12000,
+    icon: null,
+    },
+);
+
 
   function showInvoice() {
-
-    setInvoiceActive(true);
+    if(typeof window.webln === 'undefined') { 
+      noExt()
+    } else {
+      setInvoiceActive(true);
+    }
   }
   function hideInvoice() {
     setInvoiceActive(false);
   }
   
   return (
+    <>
+    <Toaster>
+    {(t) => (
+      <ToastBar
+        toast={t}
+        style={{
+          ...t.style,
+          animation: t.visible ? 'slide-in-top 1s ease' : 'slide-out-top 1s ease',
+          minWidth: "40rem",
+        }}
+      />
+    )}
+    </Toaster>
     <div className='chatComp'>
       <div className="chatInfo">
         {currentUserPubkey !== '' && <><span>{currentUserName}</span>
@@ -34,5 +63,6 @@ export const Chat = () => {
       {currentUserPubkey !== "" && <Messages></Messages>}
       {currentUserPubkey !== "" && <Input></Input>}
     </div>
+    </>
   )
 }
